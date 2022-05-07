e4ff9036858a5be0950c6d326657364cc59c2d4c
Ruihan Zhao <fouxzhao@gmail.com>
2022-05-07 16:35:53 +0100

    done
    hope it will run as my thoughts

--- js/index.js
+++ js/index.js
@@ -7,17 +7,14 @@
  * @FilePath: \tiny-heart-master\birthday-mobile\js\index.js
  */
 $("#login-button").click(function (event) {
-  var userName = document.getElementById("userName").value;
-  var pwd = document.getElementById("pwd").value;
-  //修改密码请改此处
-  if (userName == "张家阳" && pwd == "0508") {
-    event.preventDefault();
-    $("form").fadeOut(500);
-    $(".wrapper").addClass("form-success");
-    setTimeout(function () {
-      location.href = "BirthdayCake.html";
-    }, 2000);
-  } else {
-    alert("Wrong Password");
-  }
+    var userName = document.getElementById("userName").value;
+    var pwd = document.getElementById("pwd").value;
+    if (userName == "张家阳" && pwd == "0508") {
+        event.preventDefault();
+        $("form").fadeOut(500);
+        $(".wrapper").addClass("form-success");
+        document.getElementById("login-button").value=true;
+    } else {
+        alert("Wrong Password");
+    }
 });
